511418042b340a67e71d76e85bb449b98c33ed91
NoahTimmerman <101799343+NoahTimmerman@users.noreply.github.com>
2022-03-31 23:14:51 +0200

    new

--- Molecular Mechanics/src/Molecule.f90
+++ Molecular Mechanics/src/Molecule.f90
@@ -52,25 +52,25 @@ module MoleculeMod
 
         Variables%CCBondLength  = 1.507
         Variables%CHBondLength  = 1.094
-        Variables%CCBondStretch = 317
-        Variables%CHBondStretch = 300
-        Variables%ForceConstantAngle = 150
-        Variables%EquiAngle = 2
-        Variables%V1 = 50
-        Variables%n = 1.0
+        Variables%CCBondStretch = 1.326*(10.0**6)
+        Variables%CHBondStretch = 1.255*(10**6)
+        Variables%ForceConstantAngle = 475720.8
+        Variables%EquiAngle = 2.0
+        Variables%V1 = 5857.6
+        Variables%n = 2.0
         Variables%Y = 0
-        Variables%ChargeH = 1.602176/(10.0**19)
-        Variables%ChargeC = 1.602176/(10.0**19)
+        Variables%ChargeH = 0.078 * (1.602176/(10.0**19))
+        Variables%ChargeC = -0.344 * (1.602176/(10.0**19))
         Variables%Coulombs = 8987551787.0
         Variables%Avogadro = 6.022141*(10.0**23)
-        Variables%SigmaH = 1
-        Variables%SigmaC = 1
-        Variables%WellDepthH = 0.066
-        Variables%WellDepthC  = 0.033
-        Variables%r = 0.00000001
+        Variables%SigmaH = 0.600
+        Variables%SigmaC = 1.9080
+        Variables%WellDepthH = 65.688
+        Variables%WellDepthC  = 457.7296
+        Variables%r = 1.0/(10.0**5)
         Variables%Kb = 1.38064852/(10.0**23)
         Variables%Temp = 300
-
+        
     end subroutine 
 
 end module  
